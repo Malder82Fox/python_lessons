c4fb2c944f4d593c487fc69c993ed2227d4cb8c1
usha_gui <pto.nz@yandex.ru>
2023-03-21 22:11:28 +0300

    i found solving problem hw_3 and hw_4

--- hw_3.py
+++ hw_3.py
@@ -1,28 +1,14 @@
 # 3. Пользователь вводит месяц в виде целого числа от 1 до 12. Сообщить к какому времени года относится месяц
 # (зима, весна, лето, осень). Напишите решения через list и через dict.
-
 # 'winter':'spring':'summer':autumn'
 
-year = {
-    'winter': [12, 1, 2],
-    'spring': [3, 4, 5],
-    'summer': [6, 7, 8],
-    'autumn': [9, 10, 11]
-}
-user_mount = int(input('Введите номер месяца: '))
+while True:
+    user_mount = input('Введите номер месяца от 1 до 12: ')
+    if user_mount.isdigit() and 0 < int(user_mount) <= 12:
+        season_1 = ['winter', 'spring', 'summer', 'autumn', 'winter']
+        season_2 = {0: 'winter', 1: 'spring', 2: 'summer', 3: 'autumn', 4: 'winter'}
+        print(f'Месяц под номером: {user_mount}\nВ списке: {season_1[int(user_mount) // 3]}\nВ словаре:{season_2[int(user_mount) //3]}')
+        break
+    else:
+        print('Ошибка !!!')
 
-# for mount in year:
-#     print(year[mount])
-
-for quarter in (year.values()):  # перебераем значения словаря year (квартала)
-    # print(quarter)
-    for mount in quarter: # перебераем элементы значений словоря year (месяца)
-        # print(mount)
-        if user_mount == mount:
-            res = year.items(mount)
-            print(f'Ваш месяц: {res}')
-
-
-
-
-# print(year)
